66ffa615ea2f2dab03b6c6c08243d2f0fe512229
Jobe <eloh2012@gmail.com>
2026-04-19 10:26:49 -0400

    mirrored changes from normalBattle to bossBattle

--- src/routes/bossBattle.jsx
+++ src/routes/bossBattle.jsx
@@ -1409,9 +1409,13 @@ function BossBattle() {
             <div
               key={index}
               className={
-                canAct
+                canAct && i.type === "Weapon" && i.name !== equipment[0].name
                   ? `inventory-item ${i.class}`
-                  : `inventory-item disabled ${i.class}`
+                  : canAct &&
+                      i.type === "Accessory" &&
+                      i.name !== equipment[1].name
+                    ? `inventory-item ${i.class}`
+                    : `inventory-item disabled ${i.class}`
               }
               onClick={() => {
                 (i.type === "Weapon" && i.name === equipment[0].name) ||
